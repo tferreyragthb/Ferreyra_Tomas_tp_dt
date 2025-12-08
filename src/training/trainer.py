b1b4d109c1464664653821545601de585cc4400e
tferreyragthb <tferreyra@mi.unc.edu.ar>
2025-12-08 13:00:44 -0300

    Update trainer.py

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -7,26 +7,35 @@ class Trainer:
     def __init__(self, model, train_dataset, lr=1e-4, batch_size=8, device="cuda"):
         self.device = device
         self.model = model.to(device)
+
         self.loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-        self.opt = optim.Adam(model.parameters(), lr=lr)
-        self.loss_fn = nn.MSELoss()
+        # Clasificación entre N ítems → CrossEntropyLoss
+        self.loss_fn = nn.CrossEntropyLoss()
 
-        self.history = []   # 🔥 guarda pérdidas por epoch
+        self.opt = optim.Adam(model.parameters(), lr=lr)
+
+        self.loss_history = []  # para graficar
 
     def train(self, epochs=10):
+        action_dim = self.model.head.out_features
+
         self.model.train()
+
         for ep in range(epochs):
             total_loss = 0
 
             for states, actions, rtg in self.loader:
-                states = states.to(self.device)
-                actions = actions.to(self.device)
-                rtg = rtg.to(self.device)
+                states = states.to(self.device)            # (B, T)
+                actions = actions.to(self.device)          # (B, T)
+                rtg = rtg.to(self.device)                  # (B, T, 1)
 
-                pred = self.model(states, actions, rtg)
+                pred = self.model(states, actions, rtg)    # (B, T, action_dim)
 
-                loss = self.loss_fn(pred[:, :-1, :], actions[:, 1:, :])
+                loss = self.loss_fn(
+                    pred[:, :-1, :].reshape(-1, action_dim), 
+                    actions[:, 1:].reshape(-1)
+                )
 
                 self.opt.zero_grad()
                 loss.backward()
@@ -35,6 +44,6 @@ class Trainer:
                 total_loss += loss.item()
 
             avg_loss = total_loss / len(self.loader)
-            self.history.append(avg_loss)
+            self.loss_history.append(avg_loss)
 
-            print(f"Epoch {ep+1}/{epochs} - Loss: {avg_loss:.4f}")
+            print(f"Epoch {ep+1}/{epochs} – Loss: {avg_loss:.4f}")
